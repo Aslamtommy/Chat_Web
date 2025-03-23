6d201c989ae8fb6f75e102f8cd61c9aae2f4c57e
Aslamtommy <tomishelby37@gmail.com>
2025-03-24 01:20:00 +0530

    updated routes

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -64,7 +64,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ sendMessage }) => {
 
     try {
       const response = await fetch(
-        `http://localhost:5000/user/upload/${type === 'image' ? 'image' : 'audio'}`,
+        `https://chat-web-1ud8.onrender.com /user/upload/${type === 'image' ? 'image' : 'audio'}`,
         {
           method: 'POST',
           headers: {
